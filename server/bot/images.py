4d355f176239137f6b74cca47a56425f596b5f03
Catskhi <catskhi.studios@gmail.com>
2024-02-07 13:09:42 -0300

    feat: added the class Bot_Images

---
+++ server/bot/images.py
@@ -0,0 +1,11 @@
+import discord
+from discord.ext.commands import Bot
+from fastapi import UploadFile
+
+class Bot_Images():
+    def __init__(self, bot: Bot) -> None:
+        self.bot = bot
+
+    async def send_image(self, image_path: str, channel_id: int) -> None:
+        channel = self.bot.get_channel(channel_id)
+        await channel.send(file=discord.File(image_path))
