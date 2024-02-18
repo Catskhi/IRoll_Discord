14c154c13b2c1ace40d214a3db178c67545a76c9
Catskhi <catskhi.studios@gmail.com>
2024-02-18 20:24:03 -0300

    feat: Added components to the index page

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,10 +1,24 @@
 <script setup>
 
+const goToSettingsPage = async () => {
+    await navigateTo('/config/')
+}
 
 </script>
 
 <template>
-    <h1 class="bg-red-500">
-        Oi
-    </h1>
+    <div class="px-5 grid grid-cols-3 gap-x-3 mt-5">
+        <UCard>
+            <div class="flex items-center justify-center">
+                <Icon class="text-4xl" name="line-md:cog-filled-loop" />
+            </div>
+            <template #footer>
+                <div class="flex justify-center items-center">
+                    <NuxtLink to="/settings" class="text-primary text-lg hover:text-yellow-600 transition-all duration-300">
+                        Settings
+                    </NuxtLink>
+                </div>
+            </template>
+        </UCard>
+    </div>
 </template>
