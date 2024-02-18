a5fbcf1832c56f5efeb463c2c8e0f4204c38f6ab
Catskhi <catskhi.studios@gmail.com>
2024-02-18 20:25:18 -0300

    feat: Added cors

--- server/main.py
+++ server/main.py
@@ -1,17 +1,33 @@
 from fastapi import FastAPI
-from routes import messages, voice_routes, image_routes
+from routes import messages, voice_routes, image_routes, configuration
 from bot import client
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    'http://localhost:3000'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 #app routes
 app.include_router(messages.router)
 app.include_router(voice_routes.router)
 app.include_router(image_routes.router)
+app.include_router(configuration.router)
 
 @app.get("/")
 def root():
-    return {"Hello": "World"}
+    return {
+        "message": "Hello World"
+    }
 
 @app.get('/start_bot')
 async def start_bot():
