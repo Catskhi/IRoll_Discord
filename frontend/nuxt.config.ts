ed60f798c0035f0e94315d5447efb60cc6d53df3
Catskhi <catskhi.studios@gmail.com>
2024-02-18 20:15:22 -0300

    chore: Added nuxt Ui and other configs

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,8 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  colorMode: {
+    preference: 'dark'
+  },
   modules: [
-    '@nuxtjs/tailwindcss',
-    'nuxt-icon'
+    'nuxt-icon',
+    '@nuxt/ui'
   ]
 })
