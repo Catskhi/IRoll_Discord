90d8fd954195d72421e1862dfc6d0dc05a48f9e0
Catskhi <catskhi.studios@gmail.com>
2024-03-05 12:42:49 -0300

    feat: Added routes for the configuration of the bot

--- server/routes/configuration.py
+++ server/routes/configuration.py
@@ -4,12 +4,20 @@ from fastapi import APIRouter, Path
 from pydantic import BaseModel
 from settings import settings
 
-class Set_Bot_Token(BaseModel):
-    token: str
+class Set_Bot_Configs(BaseModel):
+    bot_token: str | None = None
+    npc_channel: str | None = None
 
 router = APIRouter()
 
-@router.post('/set_bot_token/')
-async def set_bot_token(data: Set_Bot_Token):
-    settings.add_new_value('token', data.token)
+@router.post('/set_bot_configs/')
+async def set_bot_token(data: Set_Bot_Configs):
+    for config in data.dict():
+        settings.add_new_value(config, data.dict()[config])
+        
     return {'success': 'successfully saved token'}
+
+@router.get('/get_bot_configs/')
+async def get_bot_configs():
+    data = settings.get_settings_data()
+    return data
