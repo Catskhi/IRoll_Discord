22aded99e293ac351e4502fa42c8ec25d679e8a7
Catskhi <catskhi.studios@gmail.com>
2024-03-21 10:46:56 -0300

    feat: start of the npc edit page

---
+++ frontend/pages/npcs/[id].vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+const route = useRoute()
+const id: number = Number(route.params.id)
+
+onBeforeMount(() => {
+   if (isNaN(Number(route.params.id))) {
+    navigateTo('/npcs/add')
+   }
+})
+
+</script>
+
+<template>
+    <h1>{{ id }}</h1>
+</template>
