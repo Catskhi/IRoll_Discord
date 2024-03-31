af183225ce00811a188860c2574d84924b59f871
Catskhi <catskhi.studios@gmail.com>
2024-03-31 19:11:33 -0300

    feat: Added function and interface to see and activate the bot status

--- frontend/pages/settings/index.vue
+++ frontend/pages/settings/index.vue
@@ -3,6 +3,8 @@
 const token = ref<string>()
 const npc_channel = ref<string>()
 const loadingSaveConfigs = ref<boolean>(false)
+const botStatus = ref<boolean>(false)
+const loadingStartBot = ref<boolean>(false)
 
 const toast = useToast()
 
@@ -43,11 +45,55 @@ const saveConfigs = async () => {
     loadingSaveConfigs.value = false
 }
 
+const getBotStatus = async () => {
+    await $fetch('http://localhost:8000/get_bot_status/', {
+        method: 'GET',
+        onResponse({response}) {
+            if (response.ok) {
+                botStatus.value = response._data.status
+            }
+        }
+    })
+}
+
+const startBot = async () => {
+    loadingStartBot.value = true
+    await $fetch('http://localhost:8000/start_bot/', {
+        method: 'GET',
+        onResponseError({response}) {
+            console.log(response)
+        }
+    })
+    setTimeout(async () => {
+        await getBotStatus()
+    }, 5000)
+    loadingStartBot.value = false
+}
+
+onBeforeMount(async () => {
+    await getBotStatus()
+})
+
 </script>
 
 <template>
     <div class="px-5 mt-5">
-        <h1 class="text-xl font-bold">Bot Settings</h1>
+        <h1 class="text-xl font-bold">Bot Settings, {{ botStatus }}</h1>
+        <div class="mt-5">
+            <div>
+                <UChip :color="botStatus ? 'green' : 'red'" size="xl"
+                    class="dark:bg-neutral-950 bg-gray-200 px-3 py-1 rounded">
+                    <p class="text-lg">
+                        Bot Status: <strong>{{ botStatus ? 'Online' : 'Offline' }}</strong>
+                    </p>
+                </UChip>
+                <UButton class="ml-5" color="emerald" :loading="loadingStartBot"
+                    @click="startBot"
+                >
+                    Start Bot
+                </UButton>
+            </div>
+        </div>
         <form class="mt-5" @submit="submitForm">
             <div class="flex flex-col w-1/2">
                 <label class="text-lg mb-1">Bot Token</label>
