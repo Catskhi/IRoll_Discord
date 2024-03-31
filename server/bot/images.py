fa9c72705c73ffedb6a44ed992551bdd80bd257c
Catskhi <catskhi.studios@gmail.com>
2024-03-30 22:55:39 -0300

    fix: Added error treatment for the function send_image_with_text

--- server/bot/images.py
+++ server/bot/images.py
@@ -12,7 +12,9 @@ class Bot_Images():
         await channel.send(file=discord.File(image_path))
 
     async def send_image_with_text(self, text: str, image_path: str, channel_id: int | None = None) -> None:
-        if not channel_id:
-            channel_id = settings.get_settings_data().npc_channel
-        channel = self.bot.get_channel(channel_id)
+        if channel_id == None and settings.get_settings_data()['npc_channel'] != '':
+            channel_id = settings.get_settings_data()['npc_channel']
+        else:
+            raise Exception("The npc channel is not configured in the settings.")
+        channel = self.bot.get_channel(int(channel_id))
         await channel.send(text, file=discord.File(image_path))
