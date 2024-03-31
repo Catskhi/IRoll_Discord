0d5437b03d625377492c261f0cc3a6a241fec343
Catskhi <catskhi.studios@gmail.com>
2024-03-30 21:40:55 -0300

    refactor: Made the channel_id property optional

--- server/bot/images.py
+++ server/bot/images.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext.commands import Bot
 from fastapi import UploadFile
+from settings import settings
 
 class Bot_Images():
     def __init__(self, bot: Bot) -> None:
@@ -10,6 +11,8 @@ class Bot_Images():
         channel = self.bot.get_channel(channel_id)
         await channel.send(file=discord.File(image_path))
 
-    async def send_image_with_text(self, text: str, image_path: str, channel_id: int) -> None:
+    async def send_image_with_text(self, text: str, image_path: str, channel_id: int | None = None) -> None:
+        if not channel_id:
+            channel_id = settings.get_settings_data().npc_channel
         channel = self.bot.get_channel(channel_id)
         await channel.send(text, file=discord.File(image_path))
