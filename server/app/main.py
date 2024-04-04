9a01e6f36e55d19f0458d7c50fb7df6979709593
Catskhi <catskhi.studios@gmail.com>
2024-04-04 09:39:45 -0300

    chore: Added a new url to the cors allowed origins on server

--- server/app/main.py
+++ server/app/main.py
@@ -8,7 +8,8 @@ from .static import verify_static_folders
 app = FastAPI()
 
 origins = [
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'http://127.0.0.1:3000'
 ]
 
 app.add_middleware(
