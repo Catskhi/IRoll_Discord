e0e273c536ad5adf1c83f1c66d29179f25a25e59
Catskhi <catskhi.studios@gmail.com>
2024-04-08 22:09:23 -0300

    fix: Removed play video testing line

--- server/app/bot/voice.py
+++ server/app/bot/voice.py
@@ -16,4 +16,3 @@ class Bot_Voice():
     async def join_voice_channel(self, channel_id: int):
         voice_channel = self.bot.get_channel(channel_id)
         voice_client = await voice_channel.connect()
-        voice_client.play(discord.FFmpegPCMAudio('https://www.youtube.com/watch?v=Y4KX-owEk98'))
