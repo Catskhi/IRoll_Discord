13d200659692055ddbf29274fa98182524f7941e
Catskhi <catskhi.studios@gmail.com>
2024-04-11 17:58:02 -0300

    feat: Added a sidebar to make the navigation throught the app more simpler.

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,5 +1,20 @@
 <script setup lang="ts">
 
+const isSlideOpen = ref<boolean>(false)
+const links = [{
+    label: 'Lobby',
+    to: '/',
+    icon: 'material-symbols:home'
+}, {
+    label: 'Settings',
+    to: '/settings/',
+    icon: 'line-md:cog-filled-loop'
+}, {
+    label: 'NPCs',
+    to: '/npcs/',
+    icon: 'material-symbols:person'
+}]
+
 const colorMode = useColorMode()
 const isDark = computed({
     get() {
@@ -20,6 +35,11 @@ const goToMainPage = async () => {
     <nav class="grid grid-cols-2">
         <div class="h-16 flex items-center pl-5 select-none"
         >
+            <div class="rounded-full p-1 hover:bg-zinc-300 dark:hover:bg-zinc-600 bg-opacity-45 cursor-pointer mr-5"
+                @click="isSlideOpen = true"
+            >
+                <Icon name="ic:round-menu" class="text-[30px]" />
+            </div>
             <h1 class="text-xl group cursor-pointer" @click="goToMainPage">
                 <span class="text-[35px] group-hover:text-red-500 transition-all duration-150">
                     <Icon name="game-icons:dice-fire" /> 
@@ -39,6 +59,20 @@ const goToMainPage = async () => {
     </nav>
     <UDivider />
     <main>
+        <USlideover v-model="isSlideOpen" side="left" class="w-44">
+            <span class="flex items-center text-[20px] px-2 space-x-2 mt-5 pb-5 select-none">
+                <Icon name="game-icons:dice-fire" class="text-[25px]" />
+                <p>
+                    IRoll Discord
+                </p>
+            </span>
+            <UDivider />
+            <UVerticalNavigation :links="links" class="mt-5" :ui="{ size: 'text-base' }">
+                <template #icon="{link}">
+                    <Icon :name="link.icon" class="text-base" />
+                </template>
+            </UVerticalNavigation>
+        </USlideover>
         <slot/>
     </main>
 </template>
