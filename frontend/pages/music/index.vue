648503f4676b1ea8a7a2ba74ff7e8c51eb701a87
Catskhi <catskhi.studios@gmail.com>
2024-04-15 11:52:26 -0300

    fix: Fixed the search and pagination functionalities

--- frontend/pages/music/index.vue
+++ frontend/pages/music/index.vue
@@ -11,12 +11,14 @@ const totalPages = ref<number>(0)
 
 const toast = useToast()
 const filteredAudios = ref({})
+const currentPageData = ref({})
 const { data: local_audios } = await useFetch<local_audios>('http://localhost:8000/audios/' , {
     server: false,
     method: 'GET',
     onResponse({response}) {
         if (response.ok) {
             filteredAudios.value = Object.fromEntries(Object.entries(response._data.audios).slice(0, page.value * 5))
+            currentPageData.value = filteredAudios.value
             totalPages.value = Object.entries(response._data.audios).length
             console.log(Object.fromEntries(Object.entries(response._data.audios).slice(0, page.value * 5)))
             return response._data.audios
@@ -31,14 +33,15 @@ const localAudioSearch = ref<string>('')
 
 watch([localAudioSearch, page], ([newSearch, newPage]) => {
     if (local_audios.value) {
-        let allFilteredAudios = Object.entries(local_audios.value?.audios).filter(([key, value]) => {
-                return key.includes(newSearch.toLowerCase()) || value.includes(newSearch.toLowerCase())
-        })
+        let allFilteredAudios = Object.entries(local_audios.value?.audios)
         if (localAudioSearch.value !== '') {
-            filteredAudios.value = Object.fromEntries(allFilteredAudios.slice(page.value - 1, page.value * 5))
-            totalPages.value = allFilteredAudios.length
+            filteredAudios.value = Object.fromEntries(allFilteredAudios.filter(([key, value]) => {
+                return key.toLowerCase().includes(newSearch.toLowerCase()) || value.toLowerCase().includes(newSearch.toLowerCase())
+            }))
+            currentPageData.value = Object.fromEntries(Object.entries(filteredAudios.value).slice((page.value - 1) * 5, page.value * 5))
+            totalPages.value = Object.entries(filteredAudios.value).length
         } else {
-            filteredAudios.value = Object.fromEntries(allFilteredAudios.slice((page.value - 1) * 5, page.value * 5))
+            currentPageData.value = Object.fromEntries(allFilteredAudios.slice((page.value - 1) * 5, page.value * 5))
             totalPages.value = Object.entries(local_audios.value.audios).length
         }
     }
@@ -51,7 +54,7 @@ watch([localAudioSearch, page], ([newSearch, newPage]) => {
     <div class="px-5 pt-5">
         <h1 class="text-xl font-bold">Music</h1>
         <div class="py-2 mt-3 rounded">
-            <div v-if="local_audios" class="flex flex-col bg-white dark:bg-gray-900 ring-1 ring-gray-200 dark:ring-gray-800">
+            <div class="flex flex-col bg-white dark:bg-gray-900 ring-1 ring-gray-200 dark:ring-gray-800">
                 <div class="px-3 py-3 grid grid-cols-2 justify-items-stretch">
                     <div class="text-lg">
                         Local Files
@@ -64,7 +67,7 @@ watch([localAudioSearch, page], ([newSearch, newPage]) => {
                         />
                     </div>
                 </div>
-                <div v-for="(audio, audio_name) in filteredAudios" 
+                <div v-if="local_audios" v-for="(audio, audio_name) in currentPageData" 
                 class="bg-white dark:bg-gray-900 ring-1 ring-gray-200 dark:ring-gray-800
                     py-3 px-3 grid grid-cols-2
                 ">
@@ -79,6 +82,9 @@ watch([localAudioSearch, page], ([newSearch, newPage]) => {
                     <div class="text-right truncate">
                         {{ audio }}
                     </div>
+                </div>
+                <div v-if="totalPages < 1" class="px-3 py-5 text-center text-opacity-50">
+                    No Results
                 </div>
                 <div class="flex items-center justify-center py-3">
                     <UPagination v-model="page" :page-count="5" :total="totalPages" size="md" />
