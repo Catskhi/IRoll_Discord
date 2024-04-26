3bace0d34ba3697f1ed5b407a66a852338fa82a2
Catskhi <catskhi.studios@gmail.com>
2024-04-25 21:24:19 -0300

    feat: Added the connection to web socket and added the toggle paused funcion

--- frontend/components/music/Player.vue
+++ frontend/components/music/Player.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
+import { v4 as uuidv4 } from 'uuid';
+
+interface player_data {
+    is_playing: boolean
+}
 
 const props = defineProps<{
     isOpened: boolean
 }>()
 
+let webSocket: WebSocket | undefined
 const isPlaying = ref<boolean>(false)
 const isPaused = ref<boolean>(false)
-const volume = ref<number>(0)
+const volume = ref<number>(100)
 const currentSongName = ref<string>('') 
 const volumeIcon = computed(() => {
     if (volume.value >= 60) {
@@ -20,14 +26,36 @@ const volumeIcon = computed(() => {
     }
 })
 
+const toggleAudioPaused = async () => {
+    await $fetch('http://localhost:8000/toggle_paused', {
+        method: 'POST',
+        onResponse({response}) {
+            if (response.ok) {
+                isPlaying.value = response._data.is_playing
+            }
+        }
+    })
+}
+
+onMounted(() => {
+    webSocket = new WebSocket(`ws://localhost:8000/voice_ws/${uuidv4()}`)
+    webSocket.onmessage = (event) => {
+        if (webSocket) {
+            const player_data = JSON.parse(event.data) as player_data
+            console.log(player_data)
+            isPlaying.value = player_data.is_playing
+        }
+    }
+})
+
 </script>
 
 <template>
-    <div class="absolute w-full bottom-0 lg:h-28 lg:pb-0 h-36 pb-3 bg-white dark:bg-gray-900 ring-2 ring-gray-200 dark:ring-gray-800
-        grid lg:grid-cols-3 lg:px-[10%]
+    <div class="fixed w-full bottom-0 lg:h-28 lg:pb-0 h-36 pb-3 bg-white dark:bg-gray-900 ring-2 ring-gray-200 dark:ring-gray-800
+        grid lg:grid-cols-3 lg:px-[10%] space-y-1
     ">
         <div class="justify-self-center self-center">
-            <p class="text-center mt-3 truncate lg:text-xl" :class="[currentSongName.length > 0 ? 'font-bold' : 'font-normal']">
+            <p class="text-center mt-3 truncate text-lg lg:text-xl" :class="[currentSongName.length > 0 ? 'font-bold' : 'font-normal']">
                 {{ currentSongName ? currentSongName : 'Select a song to play' }}
             </p>
         </div>
@@ -35,8 +63,10 @@ const volumeIcon = computed(() => {
             <UButton :ui="{ rounded: 'rounded-full' }" square class="lg:w-12 lg:h-12 h-10 w-10 flex items-center justify-center bg-gray">
                 <Icon name="ph:skip-back-fill" class="lg:text-xl text-lg" />
             </UButton>
-            <UButton :ui="{ rounded: 'rounded-full' }" square class="lg:w-12 lg:h-12 h-10 w-10 flex items-center justify-center">
-                <Icon name="ph:play-fill" class="lg:text-xl text-lg" />
+            <UButton :ui="{ rounded: 'rounded-full' }" square class="lg:w-12 lg:h-12 h-10 w-10 flex items-center justify-center"
+                @click="toggleAudioPaused"
+            >
+                <Icon :name="isPlaying == true ? 'ph:pause-fill' : 'ph:play-fill'" class="lg:text-xl text-lg" />
             </UButton>
             <UButton :ui="{ rounded: 'rounded-full' }" square class="lg:w-12 lg:h-12 h-10 w-10 flex items-center justify-center bg-gray">
                 <Icon name="ph:skip-forward-fill" class="lg:text-xl text-lg" />
