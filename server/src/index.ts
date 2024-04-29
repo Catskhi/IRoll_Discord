2cd4e6f77be880bb05e0f0c8cea80eb9ec1a9f52
Catskhi <catskhi.studios@gmail.com>
2024-04-28 21:08:37 -0300

    Moved bun application to new folder

---
+++ server/src/index.ts
@@ -0,0 +1,17 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+console.log(`Token: ${Bun.env.DISCORD_TOKEN}`)
+client.login(Bun.env.DISCORD_TOKEN);
