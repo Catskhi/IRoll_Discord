7a4d9e6cdd3f89eccf5966b87e4ffd63ded3abd8
Catskhi <catskhi.studios@gmail.com>
2024-05-17 12:28:07 -0300

    feat: Added queue command

---
+++ server/src/commands/voice/queue.ts
@@ -0,0 +1,16 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { audioPlayerHandler } from "../../handlers/AudioPlayerHandler";
+
+export const data = new SlashCommandBuilder()
+        .setName('queue')
+        .setDescription('Returns the queue.');
+
+export async function execute(interaction: ChatInputCommandInteraction) {
+    const queue = audioPlayerHandler.getQueue();
+    if (queue.length < 1) {
+        interaction.reply("The queue is empty");
+    } else {
+        let songList: string = queue.map(song => song.title).join('\n')
+        interaction.reply(`Queue: \n${"`" + songList + "`"}`);
+    }
+}
