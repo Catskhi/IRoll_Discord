86914c38b77d20ad2b6fd6cc15cf397d06ac6aca
Catskhi <catskhi.studios@gmail.com>
2024-05-18 14:03:19 -0300

    refactor: Added logic to handle the queue and loops

--- server/src/events/registerAudioPlayerEvents.ts
+++ server/src/events/registerAudioPlayerEvents.ts
@@ -5,7 +5,11 @@ import { audioPlayerHandler } from "../handlers/AudioPlayerHandler";
 export default function registerAudioPlayerEvents(player: AudioPlayer) {
     player.on(AudioPlayerStatus.Idle, (oldState, newState) => {
         console.log('Audio player is in the Idle state!');
-        if (audioPlayerHandler.positionInQueue < audioPlayerHandler.queue.length - 1) {
+        if (audioPlayerHandler.loopTrack) {
+            audioPlayerHandler.replayCurrentSong();
+            return;
+        }
+        if (audioPlayerHandler.queue.length > 0) {
             audioPlayerHandler.playNext();
         }
     });
