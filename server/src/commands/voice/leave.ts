154d3a872b42ccf4204cd5fa6976afa54e6e94b1
Catskhi <catskhi.studios@gmail.com>
2024-05-18 14:01:46 -0300

    feat: Added the leave command

--- server/src/commands/voice/leave.ts
+++ server/src/commands/voice/leave.ts
@@ -1,6 +1,6 @@
 import { getVoiceConnection } from "@discordjs/voice";
 import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
-
+import { audioPlayerHandler } from "../../handlers/AudioPlayerHandler";
 
 export const data = new SlashCommandBuilder()
     .setName('leave')
@@ -12,6 +12,8 @@ export async function execute(interaction: ChatInputCommandInteraction) {
         interaction.reply(`You're not in a voice channel.`);
         return;
     }
+    audioPlayerHandler.player?.off;
+    audioPlayerHandler.clearQueue();
     connection?.destroy();
     interaction.reply('Left voice channel.');
 }
