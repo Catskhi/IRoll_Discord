258c5d1e0bc799841a353f2b8d05107f35049445
Catskhi <catskhi.studios@gmail.com>
2024-05-30 13:24:46 -0300

    feat: Added port, cors and body parser to application

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,15 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 import { audioPlayerHandler } from './handlers/AudioPlayerHandler';
 import voice_routes from './routes/voice';
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/voice', voice_routes);
 
 app.get('/', (req: Request, res: Response) => {
