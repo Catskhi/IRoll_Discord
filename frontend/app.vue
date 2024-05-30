82ee176965d8f239150c87cc5f8ab5beee8f75b0
Catskhi <catskhi.studios@gmail.com>
2024-05-30 13:23:40 -0300

    feat: Added Nuxt page and styles to app

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap');
+body {
+  @apply bg-[#0f172a];
+}
+
+.font-inter{
+  font-family: 'Inter', sans-serif;
+}
+</style>
