df4aa10b143cc1cf6c5adb75f8d562b772228a6d
Catskhi <catskhi.studios@gmail.com>
2024-06-05 14:08:00 -0300

    refactor: Added socket io

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,18 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import http from 'http';
+import { Server } from 'socket.io';
 import { audioPlayerHandler } from './handlers/AudioPlayerHandler';
 import voice_routes from './routes/voice';
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000'
+    }
+});
 const port = 5000;
 
 app.use(cors());
@@ -16,8 +24,15 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Hi');
 })
 
+io.on('connection', (socket) => {
+    console.log(`A user connected!`);
+    audioPlayerHandler.emitCurrentState();
+})
+
 export const startApp = () => {
-    app.listen(port, () => {
+    server.listen(port, () => {
         console.log(`Access server on: http://localhost:${port}`);
     })
 }
+
+export { io };
