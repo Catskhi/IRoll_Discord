42b2a93c0c8fdf119f8b8beddf142e9696a477f4
Catskhi <catskhi.studios@gmail.com>
2024-06-05 14:06:52 -0300

    style: Added new color

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        secondary: {
+          50: '#f7f6f5',
+          100: '#ebeae9',
+          200: '#deddda',
+          300: '#bdbbb4',
+          400: '#a19d96',
+          500: '#8e8981',
+          600: '#817c75',
+          700: '#6c6762',
+          800: '#5a5652',
+          900: '#4a4644',
+          950: '#272523'
+        }
+      }
+    },
   },
   plugins: [],
 }
