e2cd6dd5f7745ad70ddc68b8d5b0b108163e4219
Catskhi <catskhi.studios@gmail.com>
2024-06-05 22:48:43 -0300

    feat: Added volume component

---
+++ frontend/components/volume.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+const volume = ref<number>(0);
+const volumeIcon = computed(() => {
+    if (volume.value == 0) {
+        return 'material-symbols:volume-off-rounded'
+    } else if (volume.value <= 20) {
+        return 'material-symbols:volume-mute-rounded'
+    } else if (volume.value <= 70) {
+        return 'material-symbols:volume-down-rounded'
+    }
+    return 'material-symbols:volume-up-rounded'
+})
+
+const changeVolume = async () => {
+    await $fetch(`http://localhost:5000/voice/volume/${volume.value}`, {
+        method: 'POST',
+        onResponse(response) {
+            console.log(response.response._data);
+        }
+    });
+}
+
+</script>
+
+<template>
+<div class="flex w-full items-center justify-center">
+    <Icon :name="volumeIcon" class="mr-3 text-[30px]" />
+    <URange @change="changeVolume" :min="0" :max="100" v-model="volume" />
+</div>
+</template>
